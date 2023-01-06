ebe4934838583b9f73afe9da994883f373f5c66a
Kelvin Kung <48689965+kelvinutp@users.noreply.github.com>
2023-01-06 11:47:45 -0500

    Update wait_4_wifi.py
    Adding redundancy in case of wrong password
    Available for choosing previously connected network or new networks

--- microcontroller(ESP8266)/wait_4_wifi.py
+++ microcontroller(ESP8266)/wait_4_wifi.py
@@ -8,65 +8,82 @@ import time
 
 print(__name__)
 
-#declare wireless object
+#declare and reset wireless object
 sta=network.WLAN(network.STA_IF)
+sta.active(False)
 sta.active(True)
+
+#save lastest 5 connected network and password
+#history={ssid:password}
+history={}
 
 def setup(ssid='',passw=''):
     '''
     Function sets ssid and password for connection
     '''
+    av=[a[0].decode('utf-8') for a in sta.scan()]#get all available wifi network in range
     
-    if ssid=='':
-        c=-1
-        while c==-1:
-            av=[a[0].decode('utf-8') for a in sta.scan()]#get all available wifi network in range
+    #try to connect to previously connected network
+    for a in history.keys():
+        if a in av:
+            __conn2wifi(a,history[a])
+        
+        if sta.isconnected():
+            break
+
+    #if not previously connected network available
+    while not(sta.isconnected()) or ssid=='':
+        while ssid=='':
+
+            #show all available networks            
             for a,b in enumerate(av):
                 print('{}. {}'.format(a,b))
+            
             c=int(input('Choose wifi network, if not available press -1: '))
             if c!=-1:
                 ssid=av[c]
-        
-
-    if passw=='':
+   
         passw=input('Enter your wifi password: ')
-
-    sta.connect(ssid,passw)
-    
-    return ssid,passw
+        __conn2wifi(ssid,passw)
  
-def loop(ssid,passw):
-    '''
-    ssid= wifi name
+    return
+    
+def __conn2wifi(ssid,passw):
+    sta.connect(ssid,passw)
+    # connecting to wifi
+    #wait until wifi connection is stablished
+    cu_time=time.time()#current time
+    while sta.isconnected()==False and time.time()!=(cu_time+10):
+        print(time.time())
+        time.sleep(1)
 
-    passd= wifi password
-    '''
-    while True:
-        connected=True
-        #wait until wifi connection is stablished
-        cu_time=time.time()#current time
+    if not(sta.isconnected()):
+        return
+    
+    #save wifi and password
+    if ssid not in history:
+        history[ssid]=passw
+    
+    #when connected print ip
+    print(sta.ifconfig())
 
-        while sta.isconnected()==False or time.time()!=(cu_time+10):
-            print(time.time())
-            time.sleep(0.5)
-            connected=False
-
-
+    return
+ 
+def loop():
+    # while True:
+    for a in range(2):
         if not(sta.isconnected()):
             setup()
 
-        #when connected print ip
-        print(sta.ifconfig())
-
         #send message
-        if not(connected):
-            print('sending message')
-            time.sleep(5)
-            ###codigo para enviar mensaje por wazap/MQTT......
-            print('message sent')
-        time.sleep(60)
+        print('sending message',a)
+        time.sleep(5)
+        ###codigo para enviar mensaje por wazap/MQTT......
+        print('message sent')
+        time.sleep(10)
 
 if __name__=='home':
-    a,b=setup()
-    loop(a,b)
-
+    setup()
+    loop()
+    for a,b in history.items():
+        print('ssid:{}\npassword:{}'.format(a,b))
